5a77ca3e244eb022fcf6a4defd8988612bb16e50
Pernot-Julien <roulio555@gmail.com>
2022-08-08 09:48:17 +0200

    add handleNumberClick

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -3,14 +3,13 @@ import './style.scss';
 
 const Calculator = () => {
  const [number, setNumber] = React.useState('');
- const handleNumberclick = (event) => {
+ const [sign, setSign ] = React.useState('');
+
+ const handleNumberClick = (event) => {
     setNumber(number => number += event.target.value);
     console.log(number);
   };
 
-const handlePointclick = (event) => {
-  setPoint(point => '.');
-};
   return(
   <div className="calculator">
     <p className="brand">caz-io</p>
@@ -18,22 +17,22 @@ const handlePointclick = (event) => {
       <div className="container">
       <button className="grid-item blue">AC</button>
       <button className="grid-item blue">+/-</button>
-      <button className="grid-item blue">%</button>  
+      <button className="grid-item blue" onClick={handleNumberClick}>%</button>  
       <button className="grid-item light-blue">/</button>
-      <button className="grid-item" onClick={handleNumberclick} value="7">7</button>
-      <button className="grid-item" onClick={handleNumberclick} value="8">8</button>  
-      <button className="grid-item" onClick={handleNumberclick} value="9">9</button>
-      <button className="grid-item light-blue">*</button>
-      <button className="grid-item" onClick={handleNumberclick} value="4">4</button>  
-      <button className="grid-item" onClick={handleNumberclick} value="5">5</button>
-      <button className="grid-item" onClick={handleNumberclick} value="6">6</button>
-      <button className="grid-item light-blue">-</button> 
-      <button className="grid-item" onClick={handleNumberclick} value="1">1</button>
-      <button className="grid-item" onClick={handleNumberclick} value="2">2</button>
-      <button className="grid-item" onClick={handleNumberclick} value="3">3</button> 
-      <button className="grid-item light-blue">+</button>
-      <button className="grid-item double" onClick={handleNumberclick} value="0">0</button>
-      <button className="grid-item" onClick={handleNumberclick} value=".">.</button> 
+      <button className="grid-item" onClick={handleNumberClick} value="7">7</button>
+      <button className="grid-item" onClick={handleNumberClick} value="8">8</button>  
+      <button className="grid-item" onClick={handleNumberClick} value="9">9</button>
+      <button className="grid-item light-blue" onClick={handleNumberClick} value='*'>*</button>
+      <button className="grid-item" onClick={handleNumberClick} value="4">4</button>  
+      <button className="grid-item" onClick={handleNumberClick} value="5">5</button>
+      <button className="grid-item" onClick={handleNumberClick} value="6">6</button>
+      <button className="grid-item light-blue" onClick={handleNumberClick} value='-'>-</button> 
+      <button className="grid-item" onClick={handleNumberClick} value="1">1</button>
+      <button className="grid-item" onClick={handleNumberClick} value="2">2</button>
+      <button className="grid-item" onClick={handleNumberClick} value="3">3</button> 
+      <button className="grid-item light-blue" onClick={handleNumberClick} value='+'>+</button>
+      <button className="grid-item double" onClick={handleNumberClick} value="0">0</button>
+      <button className="grid-item" onClick={handleNumberClick} value=".">.</button> 
       <button className="grid-item light-blue">=</button>
     </div>
   </div>

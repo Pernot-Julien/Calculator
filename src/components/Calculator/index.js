045e5cfbedb4d9f8e5fc39183b8e258ddc9da83e
Pernot-Julien <roulio555@gmail.com>
2022-08-09 14:02:10 +0200

    fix repetitive sign

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -7,37 +7,50 @@ const Calculator = () => {
 
  const handleNumberClick = (event) => {
     setNumber(number => number += event.target.value);
-    //console.log(number);
   };
 
   const handleEqualClick = (event) => {
- //parseInt(number);
- let test = eval(number);
- console.log(test);
- setEqual(equal=> test);
+    const operator = eval(number);
+    console.log(operator);
+    const substrOperator = operator.toString().substr(0, 7);
+    setEqual(equal=> substrOperator);
+  };
+
+  const handleClearButton = (event) => {
+    setNumber(number => "");
+    setEqual(equal => "");
+  };
+
+  const handleSignClick = (event) => {
+
+    const targetValue = event.target.value;
+    if ((number.slice(-1) == "+") || (number.slice(-1) == "-") || (number.slice(-1) == "/" ) || (number.slice(-1) == "*") ) {
+     setNumber(number => number.substring(0, number.length -1));
+    }
+    setNumber(number => number += targetValue);
   }
 
   return(
   <div className="calculator">
     <p className="brand">caz-io</p>
-    <div className="screen">{equal ? equal : number}</div>
+    <div className="screen">{equal ? equal : number.toString().substr(0 , 7)}</div>
       <div className="container">
-      <button className="grid-item blue">AC</button>
-      <button className="grid-item blue">+/-</button>
+      <button className="grid-item blue" onClick={handleClearButton} >AC</button>
+      <button className="grid-item blue" onClick={handleNumberClick} value="-" >+/-</button>
       <button className="grid-item blue" value='%'>%</button>  
-      <button className="grid-item light-blue" onClick={handleNumberClick} value="/">/</button>
+      <button className="grid-item light-blue" onClick={handleSignClick} value="/">/</button>
       <button className="grid-item" onClick={handleNumberClick} value="7">7</button>
       <button className="grid-item" onClick={handleNumberClick} value="8">8</button>  
       <button className="grid-item" onClick={handleNumberClick} value="9">9</button>
-      <button className="grid-item light-blue" onClick={handleNumberClick} value='*'>*</button>
+      <button className="grid-item light-blue" onClick={handleSignClick} value='*'>*</button>
       <button className="grid-item" onClick={handleNumberClick} value="4">4</button>  
       <button className="grid-item" onClick={handleNumberClick} value="5">5</button>
       <button className="grid-item" onClick={handleNumberClick} value="6">6</button>
-      <button className="grid-item light-blue" onClick={handleNumberClick} value='-'>-</button> 
-      <button className="grid-item" onClick={handleNumberClick} value="1">1</button>
+      <button className="grid-item light-blue" onClick={handleSignClick} value='-'>-</button> 
+      <button className="grid-item" onClick={handleNumberClick} value="1" >1</button>
       <button className="grid-item" onClick={handleNumberClick} value="2">2</button>
       <button className="grid-item" onClick={handleNumberClick} value="3">3</button> 
-      <button className="grid-item light-blue" onClick={handleNumberClick} value='+'>+</button>
+      <button className="grid-item light-blue" onClick={handleSignClick} value='+'>+</button>
       <button className="grid-item double" onClick={handleNumberClick} value="0">0</button>
       <button className="grid-item" onClick={handleNumberClick} value=".">.</button> 
       <button className="grid-item light-blue" onClick={handleEqualClick}>=</button>

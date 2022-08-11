aabf9d508745d9dff6f8b3db85cfdcdc08f20d15
Pernot-Julien <roulio555@gmail.com>
2022-08-11 14:16:45 +0200

    update numberSet function

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -4,14 +4,33 @@ import './style.scss';
 const Calculator = () => {
  const [number, setNumber] = React.useState('');
  const [ equal, setEqual ] = React.useState('');
+ const [lastNumber, setLastNumber ] = React.useState('');
+
+ const numberSet = (string) => {
+    const stringSplit = string.split('');
+    console.log(stringSplit, 'conversion du string en tableau');
+    for ( const i in stringSplit) {
+      console.log(i, 'numéro index');
+      console.log(stringSplit[i], "valeur de l'index");
+
+      if(stringSplit[i] == "+" || stringSplit[i] == "-" || stringSplit[i] == "*" || stringSplit[i] == "/"){
+        const numberI= i;
+        const firstSlice = stringSplit.slice(numberI, stringSplit.length);
+        const result = firstSlice.slice(1, firstSlice.length)
+        console.log(result, 'resultat de ma fonction');
+      };
+    };
+};
+
 
  const handleNumberClick = (event) => {
     setNumber(number => number += event.target.value);
+    console.log(number);
   };
 
   const handleEqualClick = (event) => {
     const operator = eval(number.replace("--", '+'));
-    console.log(operator);
+    //console.log(operator);
     const substrOperator = operator.toString().substr(0, 7);
     setEqual(equal=> substrOperator);
     setNumber(number => substrOperator);
@@ -23,23 +42,28 @@ const Calculator = () => {
   };
 
   const handleSignClick = (event) => {
-
     const targetValue = event.target.value;
     if ((number.slice(-1) == "+") || (number.slice(-1) == "-") || (number.slice(-1) == "/" ) || (number.slice(-1) == "*") ) {
      setNumber(number => number.substring(0, number.length -1));
     }
     setNumber(number => number += targetValue);
-  }
+  };
 
+  const handleNegativeClick = (event) => {
+    setNumber(number => number += event.target.value);
+    console.log(number ,'mon state number');
+    console.log(numberSet(number), "resultat final");
+    
+    };
 
-
+ // console.log(numberSet ("1+56+338"));
   return(
   <div className="calculator">
     <p className="brand">caz-io</p>
     <div className="screen">{number ? number : equal}</div>
       <div className="container">
       <button className="grid-item blue" onClick={handleClearButton} >AC</button>
-      <button className="grid-item blue" onClick={handleNumberClick} value="-" >+/-</button>
+      <button className="grid-item blue" onClick={handleNegativeClick} value="-" >+/-</button>
       <button className="grid-item blue" value='%'>%</button>  
       <button className="grid-item light-blue" onClick={handleSignClick} value="/">/</button>
       <button className="grid-item" onClick={handleNumberClick} value="7">7</button>
